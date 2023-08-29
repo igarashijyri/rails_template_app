abb1f044d982e9f788fee349c3107c1aa729a249
igarashijyri <fourleafclover688@gmail.com>
2023-08-29 21:21:59 +0900

    feature: Active AdminでUserモデルのレコードを生成できるようにした

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,30 @@
 ActiveAdmin.register User do
-  permit_params :email, :encrypted_password
+  permit_params :name, :email, :password, :password_confirmation, :current_password
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :encrypted_password
+    column :created_at
+    actions
+  end
+
+  filter :email
+  filter :name
+  filter :created_at
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      if f.object.persisted?
+        f.input :current_password
+      end
+    end
+    f.actions
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    super + [ 'email' ]
+  end
 end
